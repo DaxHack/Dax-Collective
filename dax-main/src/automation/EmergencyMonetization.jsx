00fc6c680bea753f2a76922c54756bb7c4c8f711
Dax R <daxdaniel2013@gmail.com>
2025-06-09 17:59:39 -0400

    save: Current working state before adding refined components

--- dax-main/src/automation/EmergencyMonetization.jsx
+++ dax-main/src/automation/EmergencyMonetization.jsx
@@ -2,14 +2,9 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion';
 import {
-  BoltIcon,
   FireIcon,
-  CurrencyDollarIcon,
-  ClockIcon,
-  RocketLaunchIcon,
-  ChartBarIcon,
   ExclamationTriangleIcon,
-  CheckCircleIcon
+  ClockIcon
 } from '@heroicons/react/24/outline';
 
 const EmergencyMonetization = () => {
