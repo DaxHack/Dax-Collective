fdff075795078a0627b6aef0f0a47395a5ab30be
Dax R <daxdaniel2013@gmail.com>
2025-06-10 20:36:19 -0400

    About to add Auth for Dev

--- dax-main/src/pages/Dashboard.jsx
+++ dax-main/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 // src/pages/Dashboard.jsx
 import React from 'react';
 import AutomationDashboard from '../automation/AutomationDashboard';
+//import { Helmet } from 'react-helmet-async'
 
 const Dashboard = () => {
   return <AutomationDashboard />;
